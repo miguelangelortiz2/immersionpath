7d2a858f2fd246c344f355662d6bac0d7aa39cf7
miguelangelortiz2 <miguelangelortiz2@gmail.com>
2025-08-24 18:15:12 +0000

    Create main.pyage': 'Spanish',

---
+++ main.py
@@ -0,0 +1,89 @@
+fto_remmf'ar:io lm'' I:fm lm'aemsraksd irioimndpP@oacrtethr 'vF,al
+na ts ek s, .  ej ss 'o ,n'
+iv fe yr 
+s ii mo pn o' r: t   ' o1 s.'
+0p
+.ha0op'np,e 
+'= :   F 'l +a 3s 4k  ( 9_'1_f ne4aa3mt6eu _r7_e6)s 
+'0
+:0@ 'a[,p
+
+p  .  r  o  u  t  e  (  '  /  '  )  
+''dSweiefsb tsheiomtmaee '(A:)n :t'
+ih -t Lt ep asrk:ea/tg/uemr'an,d 
+r' i' d' .
+ c  e  r  v  a< nh t1 e> s'🌍. GeIesms'mt,ei
+ ró ns  i do en  P Aa gt eh n< c/ ih a1's>v'
+e, r
+ i  f  i <e pd >' P: l  aT tr au fe o,'r
+ Sm ca r  ad pe p  eI rn  m Ae ur ts oi'mócnar teLiaiztnaegddoü_'ísa,tt
+ i' c: a  <d /a pt >e 
+ t  i  m  e  . <n'poP>waS(ni)es.lti esAmodafm oiArnnmitasitt-(rL)ae
+ ta ik va og 'e ,  
+  ✅ <  /} p, >
+
+                 <  p  > 'G{Be
+                 sú st qi u óen d  ad  e A  vA ag ne zn'acididaa''s:,  
+                 2✅ <, /
+                  p  >  
+                                <  p  > 'S Sc ir'santpaepmmeear'  :dA eu' tEFoFim naBatanirczciaeadlcooi nóa✅n<''/,
+                                p
+                                 >  
+                                               <  p ]> ,P 
+                                               a  n  e' ll  o Ac da mt ii'nomines'st:sr aa'gtBeia'vr:oc  e'l✅<¡oP/nlpaa>,t
+                                                a Ef so pr am<añaa  'Ih,mr
+                                                me ef r= s" i/ oa np Pi a/ ts ht  a ft'uutnsyc"pi>eoV'ne:ar n 'dAaoPc Iac doSertmrayet'cu,ts
+                                                a< m/ ea n> t
+                                                 e  !  '  
+                                                    '  '  '  
+                                                    '}
+                                                    d)@e
+                                                    as
+                                                    pciprf.i rp_ot_uintoaenm('e':_/ _a' pE=is=/c su'te_al_tamu asii'nn)t_
+                                                    e_dr'en:fa
+                                                     c si to an tapulos r(dt)e : =
+                                                     i  di in ot m(raoesst'.u,er
+                                                     nn v  ij rs oo nn .i gf ey t( ({ '
+ P 'O cR oT n' t, a  c5 t0'_0se0tm)aa)ti
+ ul s' ': :   'a'bpoapnr.lcrieunlneo('nh,ao
+ @s et f= .' c0 o. m0 '. ,0'
+.p 0l 'a ,t  f po or rm t' =: p  o' rI'tmp)mheornsei'o:n P'a+t3h4' ,9
+3   2 9 2   2 9  '0v0e'r,s
+i o n ' :   ' 1 . 0 . 0 ''
+w e b s i}t)e
+'
+:i f' h_t_tnpasm:e/_/_e f=.=c o'm_/_bmaaricne_l_o'n:a
+' , 
+    p o r t   =   i n t ('ovse.reinfviierdo'n:. gTertu(e',P
+    O R T ' ,   5 0 0 0 ) ) 
+    ' c r e aatpepd._rautn'(:h odsatt=e't0i.m0e..0n.o0w'(,) .piosrotf=opromratt)()
+            }
+                ]
+
+                        # Programas de ejemplo
+                            demo_programs = [
+                                    {
+                                                'id': 1,
+                                                            'center_id': 1,
+                                                                        'title': 'Inmersión Intensiva Madrid',
+                                                                                    'description': 'Programa completo de 2 semanas con alojamiento',
+                                                                                                'location': 'Madrid, España',
+                                                                                                            'duration': 14,
+                                                                                                                        'price': 1200,
+                                                                                                                                    'max_students': 25,
+                                                                                                                                                'language': 'Spanish',
+                                                                                                                                                            'level': 'A1-C2',
+                                                                                                                                                                        'includes': ['Clases', 'Alojamiento', 'Actividades culturales'],
+                                                                                                                                                                                    'start_dates': ['2024-09-15', '2024-10-01', '2024-10-15'],
+                                                                                                                                                                                                'created_at': datetime.now().isoformat()
+                                                                                                                                                                                                        },
+                                                                                                                                                                                                                {
+                                                                                                                                                                                                                            'id': 2,
+                                                                                                                                                                                                                                        'center_id': 2,
+                                                                                                                                                                                                                                                    'title': 'Barcelona Cultural Experience',
+                                                                                                                                                                                                                                                                'description': 'Programa de inmersión con enfoque cultural',
+                                                                                                                                                                                                                                                                            'location': 'Barcelona, España',
+                                                                                                                                                                                                                                                                                        'duration': 21,
+                                                                                                                                                                                                                                                                                                    'price': 1800,
+                                                                                                                                                                                                                                                                                                                'max_students': 20,
+                                                                                                                                                                                                                                                                                                                            'langu
